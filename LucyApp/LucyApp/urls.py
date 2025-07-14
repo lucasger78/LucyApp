a2530e8d7f39fc384b8b4ec85198bafdb360a33c
lucasger78 <lucasger78@hotmail.com>
2025-07-14 01:48:02 -0300

    finalizada, falta css a full

---
+++ LucyApp/LucyApp/urls.py
@@ -0,0 +1,42 @@
+"""
+URL configuration for LucyApp project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.contrib.auth.views import LoginView, LogoutView  # Importa LoginView y LogoutView
+# from django.views.generic import RedirectView
+
+# urlpatterns = [
+#     path('', RedirectView.as_view(url='/login/', permanent=False)),
+#     path('', LoginView.as_view(template_name='registration/login.html'), name='login'),
+#     path('logout/', LogoutView.as_view(), name='logout'),
+#     path('admin/', admin.site.urls),
+#     path('accounts/', include('django.contrib.auth.urls')),
+#     path('pedidos/', include('pedidos.urls')),
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('catalogo/', include('pedidos.urls')),  # Asegúrate que esto apunte a tu app
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
